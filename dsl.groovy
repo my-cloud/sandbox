ea3615d6afeaddce4a91c79f2e204cf64c5a40d2
root <gwenall.pansier@photobox.com>
2017-04-25 18:35:13 +0100

    test

--- dsl.groovy
+++ dsl.groovy
@@ -1,10 +1,4 @@
 job('example-job-from-job-dsl') {
-    scm {
-        github('my-cloud/sandbox', 'test')
-    }
-    triggers {
-        cron("@hourly")
-    }
     steps {
         shell("echo 'Hello World'")
     }
