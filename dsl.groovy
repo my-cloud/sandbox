8706fb26f2ec45dcb0a7d1330337be5201514b99
root <gwenall.pansier@photobox.com>
2017-04-25 18:33:44 +0100

    test

--- dsl.groovy
+++ dsl.groovy
@@ -1,37 +1,11 @@
-'''job('example') {
+job('example-job-from-job-dsl') {
     scm {
-        git {
-            remote {
-                github('my-cloud/sandbox')
-                refspec('+refs/pull/*:refs/remotes/origin/pr/*')
-            }
-            branch('${sha1}')
-        }
+        github('my-cloud/sandbox', 'test')
     }
     triggers {
-        githubPullRequest {
-            admin('my-cloud-bot')
-            userWhitelist('my-cloud-bot')
-            orgWhitelist('my-cloud-bot')
-            cron('H/5 * * * *')
-            triggerPhrase('ok to test')
-            onlyTriggerPhrase()
-            useGitHubHooks()
-            permitAll()
-            autoCloseFailedPullRequests()
-            allowMembersOfWhitelistedOrgsAsAdmin()
-            extensions {
-                commitStatus {
-                    context('deploy to staging site')
-                    triggeredStatus('starting deployment to staging site...')
-                    startedStatus('deploying to staging site...')
-                    statusUrl('http://google.com')
-                    completedStatus('SUCCESS', 'All is well')
-                    completedStatus('FAILURE', 'Something went wrong. Investigate!')
-                    completedStatus('PENDING', 'still in progress...')
-                    completedStatus('ERROR', 'Something went really wrong. Investigate!')
-                }
-            }
-        }
+        cron("@hourly")
     }
-}'''
+    steps {
+        shell("echo 'Hello World'")
+    }
+}
